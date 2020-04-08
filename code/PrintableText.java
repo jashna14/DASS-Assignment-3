95352e1ada5959101dbc4a268e6e70e61d2cb325
SahiltheGreat <smbstar1@gmail.com>
2020-04-08 11:50:02 +0530

    edit

--- code/PrintableText.java
+++ code/PrintableText.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.awt.print.*;
 import java.awt.geom.*;
 import java.awt.font.*;
-import java.text.*;
 
 public class PrintableText implements Printable {
 	String text;
@@ -17,29 +16,62 @@ public class PrintableText implements Printable {
 		text = t;
 	}
 
-	public int print(Graphics g, PageFormat pageFormat, int pageIndex) {
-		if (pageIndex > 0) {
+	public int NosuchPage(int pageIndex) {
+		if(pageIndex > 0) {
 			return NO_SUCH_PAGE;
 		}
+		return pageIndex;
+	}
+
+	public double rettemp() {
+		double tempval = 0.25 * POINTS_PER_INCH;
+		return tempval;
+	}
+
+	public int TempsumValOneReturn(int j) {
+		return j * 14;
+	}
+
+	public void loopfunction(Graphics2D g2d,FontRenderContext frc) {
+		Font font = new Font("courier", Font.PLAIN, 12);
+		String[] lines = text.split("\n");
+		double tempvalue = rettemp();
+		Point2D.Double pen = new Point2D.Double(tempvalue, tempvalue);
+		for (int i = 0; i < lines.length; i++) {
+			if (lines[i].length() > 0) {
+				TextLayout layout = new TextLayout(lines[i], font, frc);
+				double tempsum = pen.y + TempsumValOneReturn(i);
+				layout.draw(g2d, (float) pen.x, (float) (tempsum));
+			}
+		}
+	}
+
+	public int print(Graphics g, PageFormat pageFormat, int pageIndex) {
+//		if (pageIndex > 0) {
+//			return NO_SUCH_PAGE;
+//		}
+		NosuchPage(pageIndex);
 
 		Graphics2D g2d = (Graphics2D) g; // Allow use of Java 2 graphics on
 
 		g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
 		g2d.setPaint(Color.black);
 
-		Point2D.Double pen = new Point2D.Double(0.25 * POINTS_PER_INCH, 0.25 * POINTS_PER_INCH);
+//		Point2D.Double pen = new Point2D.Double(0.25 * POINTS_PER_INCH, 0.25 * POINTS_PER_INCH);
 
-		Font font = new Font ("courier", Font.PLAIN, 12);
-		FontRenderContext frc = g2d.getFontRenderContext();
+//		Font font = new Font ("courier", Font.PLAIN, 12);
+   		FontRenderContext fac = g2d.getFontRenderContext();
+   		loopfunction(g2d,fac);
 
-		String lines[] = text.split("\n");
+//		String[] lines = text.split("\n");
 
-		for (int i=0; i < lines.length; i++) {
-			if (lines[i].length() > 0) {
-				TextLayout layout = new TextLayout(lines[i], font, frc);
-				layout.draw(g2d, (float) pen.x, (float) (pen.y + i*14));
-			}
-		}
+//		for (int i=0; i < lines.length; i++) {
+//			if (lines[i].length() > 0) {
+//				TextLayout layout = new TextLayout(lines[i], font, frc);
+//				double tempsum = pen.y + i*14;
+//				layout.draw(g2d, (float) pen.x, (float) (tempsum));
+//			}
+//		}
 
 		return PAGE_EXISTS;
 	}
